52039afe87040374eba0e06793e52b229c74ce73
Salome Aufrecht <salome.aufrecht@tngtech.com>
2025-10-31 13:32:17 +0100

    z

--- tests/test_orange2.py
+++ tests/test_orange2.py
@@ -2,4 +2,4 @@ import pytest
 import random
 
 def test_flaky_example():
-    assert random.choice([True, False]) or random.choice([True, False]) or random.choice([True, False])  # Zufällig erfolgreich/scheiternd
+    assert random.choice([True, False]) or random.choice([True, False])   # Zufällig erfolgreich/scheiternd
