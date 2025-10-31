628b80728e16e6a450b1b9ad7e9dfb093b99bba1
Salome Aufrecht <salome.aufrecht@tngtech.com>
2025-10-31 12:49:51 +0100

    more tests

---
+++ tests/test_fine copy.py
@@ -0,0 +1,5 @@
+import pytest
+import random
+
+def test_flaky_example():
+    assert True  
---
+++ tests/test_flaky copy 2.py
@@ -0,0 +1,5 @@
+import pytest
+import random
+
+def test_flaky_example():
+    assert random.choice([True, False])  # Zufällig erfolgreich/scheiternd
