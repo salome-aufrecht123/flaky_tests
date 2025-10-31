b187d45eac3503056d5c0a9bed3527b39efe9297
Salome Aufrecht <salome.aufrecht@tngtech.com>
2025-10-31 13:20:08 +0100

    add newline at end of file in test_orange3.py

--- tests/test_orange3.py
+++ tests/test_orange3.py
@@ -3,3 +3,6 @@ import random
 
 def test_flaky_example():
     assert random.choice([True, False]) or random.choice([True, False])  # Zufällig erfolgreich/scheiternd
+
+def test_always_passes():
+    assert True
